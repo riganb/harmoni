c0ab2f58740dd3e999441e5b1abe0a08dcb2418c
Rigan Burnwal <therealriganb@gmail.com>
2024-01-16 21:05:35 +0530

    adding search functionality to doctors section

--- app/tabs/doctors.tsx
+++ app/tabs/doctors.tsx
@@ -5,6 +5,7 @@ import {
   doctors_header_artifact,
 } from "../../assets/images";
 import clsx from "clsx";
+import { useMemo, useState } from "react";
 
 export const DOCTOR_CARDS_BORDER_COLOURS = [
   "border-yellow-500",
@@ -97,9 +98,17 @@ const DoctorCard = ({ doctor }: { doctor: Doctor }) => {
 };
 
 export default function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredDoctors = useMemo(
+    () =>
+      SAMPLE_DOCTORS_DATA.filter((doctor) => doctor.name.includes(searchTerm)),
+    [searchTerm]
+  );
+
   return (
     <View className="flex-1 p-5">
-      <View className="flex-1 border-2 border-gray-200 rounded-3xl w-full justify-center items-center my-3">
+      <View className="flex-1 rounded-3xl w-full justify-center items-center my-3">
         <View className="px-3 w-full">
           <View className="absolute pl-5 pt-8">
             <Icon source="text-search" size={24} color={MD3Colors.neutral50} />
@@ -107,12 +116,13 @@ export default function App() {
           <TextInput
             placeholder="Doctors"
             className="w-full py-2 mt-5 px-2 pl-11 text-lg rounded-2xl border-2 border-gray-300"
+            onChangeText={setSearchTerm}
           />
         </View>
         <Image source={doctors_header_artifact} className="m-5" />
       </View>
       <ScrollView className="pl-5 gap-5 flex-1 mt-3">
-        {SAMPLE_DOCTORS_DATA.map((doctor) => {
+        {filteredDoctors.map((doctor) => {
           return <DoctorCard doctor={doctor} key={doctor.id} />;
         })}
       </ScrollView>
