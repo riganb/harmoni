37d386eb5b913a43b55fd7e6e5b48367c1f55d2e
Rigan Burnwal <therealriganb@gmail.com>
2024-01-17 10:24:58 +0530

    completing index screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,25 @@
 import { router } from "expo-router";
-import { useEffect } from "react";
-import { View, SafeAreaView, StatusBar } from "react-native";
+import { useCallback, useEffect } from "react";
+import { View, Image } from "react-native";
 import { Button, Text } from "react-native-paper";
+import { home_card_artifact } from "../assets/images";
 
 export default function App() {
-  useEffect(() => {
-    setTimeout(() => router.push("/tabs/home"), 1000);
-  });
+  const beginJourney = useCallback(() => {
+    router.push("/tabs/home");
+  }, []);
+
   return (
     <View className="flex-1 justify-center items-center">
-      <Text>Hello World</Text>
-      <Button icon="camera" mode="contained">
-        Hi Fellas
+      <Image source={home_card_artifact} />
+      <Text variant="headlineLarge">Welcome to Harmoni</Text>
+      <Button
+        className="mt-5"
+        icon="arrow-expand-right"
+        mode="contained"
+        onPress={beginJourney}
+      >
+        Let's begin the journey, shall we?
       </Button>
     </View>
   );
